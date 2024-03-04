8d2cb7f97a91bf8ae5d5d21946a1051f1bc059e2
Ed <silva.email@yahoo.com>
2024-03-04 12:48:01 -0500

    Refactor button hover functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,24 +2,32 @@ import React, { useState } from "react";
 
 function App() {
   const [headingText, setHeadingText] = useState("Hello There My Friend!");
-
-  
+  const [isMouseOver, setMouseOver] = useState(false);
 
   const handleClick = () => {
     console.log("I have been clicked!");
     setHeadingText("Submitted");
   };
 
-  const btnHover = () => {
-    if (onmouseover) {
-      document.querySelector(button);
-    }
+  const handleMouseOver = () => {
+    setMouseOver(true);
+    console.log("I'm roving over!!!!");
   };
+
+  const handleMouseOut = () => {
+    setMouseOver(false);
+  };
+
   return (
     <div className="container">
       <h1>{headingText}</h1>
       <input type="text" placeholder="What's your name?" />
-      <button style={{ backgroundColor: "white" }} onClick={handleClick}>
+      <button
+        style={{ backgroundColor: isMouseOver ? "black" : "white" }}
+        onClick={handleClick}
+        onMouseOver={handleMouseOver}
+        onMouseOut={handleMouseOut}
+      >
         Submit
       </button>
     </div>
