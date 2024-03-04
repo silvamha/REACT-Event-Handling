f8f8a2b1c91a4ec0b40b32830178aad3e9460a1f
Ed <silva.email@yahoo.com>
2024-03-04 12:24:18 -0500

    Add button hover effect

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,15 +3,25 @@ import React, { useState } from "react";
 function App() {
   const [headingText, setHeadingText] = useState("Hello There My Friend!");
 
+  
+
   const handleClick = () => {
     console.log("I have been clicked!");
     setHeadingText("Submitted");
+  };
+
+  const btnHover = () => {
+    if (onmouseover) {
+      document.querySelector(button);
+    }
   };
   return (
     <div className="container">
       <h1>{headingText}</h1>
       <input type="text" placeholder="What's your name?" />
-      <button onClick={handleClick}>Submit</button>
+      <button style={{ backgroundColor: "white" }} onClick={handleClick}>
+        Submit
+      </button>
     </div>
   );
 }
