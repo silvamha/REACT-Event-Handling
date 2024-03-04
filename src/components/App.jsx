9aab19394c868c09d9d62e0c27fc032d4f1a42b6
Ed <silva.email@yahoo.com>
2024-03-04 08:43:44 -0500

    Add state and event handling to App component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [headingText, setHeadingText] = useState("Hello There My Friend!");
+
+  const handleClick = () => {
+    console.log("I have been clicked!");
+    setHeadingText("Submitted");
+  };
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>{headingText}</h1>
       <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <button onClick={handleClick}>Submit</button>
     </div>
   );
 }
